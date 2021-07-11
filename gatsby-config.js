be87822747ef981b496b6603862be8068b866842
Mike Babetchki <mike.babetchki@gmail.com>
2021-07-11 05:46:24 -0500

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,29 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "portfolio-revamp",
+    siteUrl: "https://www.mbabetchki.com",
+    title: "MB Web Development",
+    author: "Mike Babetchki",
+    description: "A portfolio website showcasing my recent work.",
   },
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-material-ui`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /assets/,
+        },
+      },
+    },
+  ],
 };
